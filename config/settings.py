8cb6c42d5035782f208b779e16e25cf4ae99de83
임서현 <limsh255@gmail.com>
2026-08-15 18:29:40 +0900

    기본 세팅 변경

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+
 from pathlib import Path
 import os
 from dotenv import load_dotenv
